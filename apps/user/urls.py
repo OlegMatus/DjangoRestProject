caadc2653dad35ec5346c9ea5f491bc2cee5e98f
OlegMatus <129312840+OlegMatus@users.noreply.github.com>
2025-01-08 01:46:53 +0200

    lesson/hw5

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import UserListCreateView
+from .views import UserListCreateView, BlockUserView, UnBlockUserView, UserToAdminView
 
 urlpatterns = [
     path('', UserListCreateView.as_view(), name='user_list_create'),
+    path('/<int:pk>/block', BlockUserView.as_view(), name='block_user'),
+    path('/<int:pk>/unblock', UnBlockUserView.as_view(), name='unblock_user'),
+    path('/<int:pk>/to_admin', UserToAdminView.as_view(), name='user_to_admin'),
 ]
